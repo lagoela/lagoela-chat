aea3dba42e5e2f6396128dd1536f7ad9b18ead43
Enzo Lagoela <lagoela.dev@gmail.com>
2024-02-02 20:09:36 -0300

    Server created, type set to modules

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("HELLO WORLD");
+import express from "express";
+import { createServer } from "node:http";
+
+const app = express();
+const server = createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!<h1>");
+});
+
+server.listen(3000, (serverInfo) => {
+    console.log('Listening on port 3000!');
+})
