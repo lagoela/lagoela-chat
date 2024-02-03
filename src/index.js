d8122b8720205a04ad384160237188538e2b3e8e
Enzo Lagoela <lagoela.dev@gmail.com>
2024-02-02 23:57:29 -0300

    Added socket.io emits, changed HTML, add messages on screen

--- src/index.js
+++ src/index.js
@@ -12,17 +12,19 @@ const io = new Server(server);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
-res.sendFile(join(__dirname, '../public/index.html'))
-
+  res.sendFile(join(__dirname, "../public/index.html"));
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  })
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit('chat message', msg)
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 server.listen(3000, (serverInfo) => {
-    console.log('Listening on port 3000!');
-})
+  console.log("Listening on port 3000!");
+});
